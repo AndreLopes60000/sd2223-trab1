135f8aeb6582dd9f0848fe5de56e45d290ce2a69
João_Pereira_60974 <jpb.pereira@campus.fct.unl.pt>
2023-04-18 16:07:40 +0100

    Discovery added and feeds altered

---
+++ src/sd2223/trab1/servers/FeedsServer.java
@@ -0,0 +1,47 @@
+package sd2223.trab1.servers;
+
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import sd2223.trab1.servers.resources.UsersResource;
+
+import java.net.InetAddress;
+import java.net.URI;
+import java.util.logging.Logger;
+
+    public class FeedsServer {
+    private static Logger Log = Logger.getLogger(UsersServer.class.getName());
+
+    static {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+    }
+
+    public static final int PORT = 8080;
+    public static final String SERVICE = "FeedsService";
+    private static final String SERVER_URI_FMT = "http://%s:%s/rest";
+
+        public static void main(String[] args) {
+            try {
+
+                ResourceConfig config = new ResourceConfig();
+                config.register(UsersResource.class);
+                // config.register(CustomLoggingFilter.class);
+
+                String ip = InetAddress.getLocalHost().getHostAddress();
+                String serverURI = String.format(SERVER_URI_FMT, ip, PORT);
+
+                Discovery discovery = aula3.api.Discovery.getInstance();
+                discovery.announce(SERVICE, serverURI);
+
+                JdkHttpServerFactory.createHttpServer(URI.create(serverURI), config);
+
+
+                Log.info(String.format("%s Server ready @ %s\n", SERVICE, serverURI));
+
+                // More code can be executed here...
+            } catch (Exception e) {
+                Log.severe(e.getMessage());
+            }
+        }
+
+
+}
